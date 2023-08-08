ae3def9c096030d1d1cf61945bcac7bd8898bf7d
BingBlingBoy <ckeiranbarnes@gmail.com>
2023-08-08 14:26:19 +0100

    Fixed some bug issues

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "rust", "typescript","c", "lua", "vim", "vimdoc", "query", "tsx", "html" },
+  ensure_installed = { "javascript", "rust", "typescript","c", "lua", "vim", "vimdoc", "query", "tsx", "html", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -8,7 +8,12 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
+  
+  indent = {
+    enable = true,
+    disable = { "python", },
+  },
+    -- ensure_installed = { "lua" },
 
   highlight = {
     enable = true,
--- lua/ckbarnz/packer.lua
+++ lua/ckbarnz/packer.lua
@@ -49,7 +49,7 @@ return require('packer').startup(function(use)
 --  use {'neoclide/coc.nvim', branch = 'release'}
   
   -- Auto completion and snippets
-  use ('h`rsh7th/nvim-cmp')
+  use ('hrsh7th/nvim-cmp')
   use ('hrsh7th/cmp-nvim-lsp')
   use ('L3MON4D3/LuaSnip')
   use ('saadparwaiz1/cmp_luasnip')
